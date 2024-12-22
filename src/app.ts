cb7f6cde1762e4edc96fbe5578cda17de3649013
Nazmul Hoque <sujon686500@gmail.com>
2024-12-22 12:01:18 +0600

    deploy check

--- src/app.ts
+++ src/app.ts
@@ -12,11 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [
-      'http://localhost:5173',
-      'http://localhost:3000',
-      'https://blog-management-backend-iota.vercel.app',
-    ],
+    origin: ['http://localhost:5173', 'http://localhost:3000'],
   }),
 );
 
