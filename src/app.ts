1a6273473a3f708d8d58cd86ff942d71f1b4f7b1
Nazmul Hoque <sujon686500@gmail.com>
2024-12-22 11:35:05 +0600

    check prod

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,15 @@ const app: Application = express();
 //parsers
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:3000'] }));
+app.use(
+  cors({
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:3000',
+      'https://blog-management-backend-iota.vercel.app',
+    ],
+  }),
+);
 
 // application routes
 app.use('/api', router);
